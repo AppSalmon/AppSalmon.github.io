6842ae8ba95f3dd521292d02a56dd3c40e461bd4
AppSalmon <hoangtuan.salmon@gmail.com>
2023-09-05 00:43:14 +0700

    test background

--- pic/lifemap/resize.py
+++ pic/lifemap/resize.py
@@ -1,10 +1,10 @@
 from PIL import Image
 
 # Mở ảnh
-image = Image.open("background_main_transparent.png")
+image = Image.open("fire.gif")
 
 # Thay đổi kích thước
 image_resized = image.resize((1200, 630))
 
 # Lưu ảnh đã thay đổi kích thước
-image_resized.save("background_main_test.png")
+image_resized.save("background.gif")
